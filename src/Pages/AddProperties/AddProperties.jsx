2da22fb4fba6936b5d7ec3fcdb82fee86806c3ea
kantho202 <kanthochakma2025@gmail.com>
2025-11-10 23:46:19 +0600

    navbar-updated

--- src/Pages/AddProperties/AddProperties.jsx
+++ src/Pages/AddProperties/AddProperties.jsx
@@ -1,32 +1,63 @@
 import React, { use } from 'react';
 import { AuthContext } from '../../context/AuthContext';
+import Swal from 'sweetalert2';
 
 const AddProperties = () => {
-    const {user}=use(AuthContext)
-    const handleAddProperties =(e)=>{
+    const { user } = use(AuthContext)
+    const handleAddProperties = (e) => {
         e.preventDefault()
-        const propertiesName =e.target.propertyName.value;
-        const description =e.target.description.value;
-        const price =e.target.price.value;
-        const location =e.target.propertyName.value;
-        console.log(propertiesName,description,price,location)
+        const propertiesName = e.target.propertyName.value;
+        const description = e.target.description.value;
+        const price = e.target.price.value;
+        const location = e.target.propertyName.value;
+        console.log(propertiesName, description, price, location)
+
+        const newProperties = {
+            property_name: propertiesName,
+            description: description,
+            property_price: price,
+            location: location
+        }
+
+        fetch('http://localhost:3000/addProperties', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(newProperties)
+        })
+            .then(res => res.json())
+            .then(data => {
+                if (data.insertedId) {
+                    Swal.fire({
+                        position: "top-center",
+                        icon: "success",
+                        title: "Properties has been saved",
+                        showConfirmButton: false,
+                        timer: 1500
+                    });
+                }
+            })
+
 
     }
     return (
-        <div className="hero bg-base-200 min-h-screen p-20  mx-auto">
+       <div className='bg-gray-200'>
+         <div className="hero bg-base-200 min-h-screen p-20  mx-auto">
             <div className="hero-content flex-col lg:flex-row shrink-0 shadow-2xl  rounded-2xl">
                 <div className="text-center w-full lg:text-left">
 
-                    <img src="/src/assets/premium_photo-1676968002512-3eac82b1d847.avif" className='mx-auto rounded-2xl hidden lg:flex ' alt="" />
+                    <img src="/src/assets/Black And Grey Real Estate Instagram Post.png"
+                        className='mx-auto rounded-2xl hidden lg:flex opacity-100 ' alt="" />
                 </div>
                 <div className="card bg-base-100 w-full  ">
                     <div className="card-body">
                         <h1 className="text-2xl lg:text-3xl text-center  font-bold">Add Properties!</h1>
                         <p className='text-primary font-medium text-center '>Add your property easily and reach real buyers. Start your journey with Home Nest today.</p>
-                       <form onSubmit={handleAddProperties} >
-                         <fieldset className="fieldset">
+                        <form onSubmit={handleAddProperties} >
+                            <fieldset className="fieldset">
 
-                            {/* name
+                                {/* name
                             <div className='flex '>
                                 <label className="label mr-31">First Name</label>
                                 <label className="label">Last Name</label>
@@ -38,44 +69,62 @@ const AddProperties = () => {
                                 <input type="text" className="input rounded-full w-full" name='name ' placeholder="Last Name" />
                             </div> */}
 
-                             {/* name */}
-                            <label className="label">User Name</label>
-                            <input type="text" name='name' className="input rounded-full w-full " placeholder={user?.displayName} />
-                            {/* email */}
-                            <label className="label">Email Address</label>
-                            <div className='flex items-center relative '>
-                                {/* <MdOutlineMailOutline size={22} className='text-gray-600 absolute top-1/2 left-3 -translate-y-1/2' /> */}
-                                <input type="email"
-                                    className="input rounded-full w-full " name='email '
-                                    placeholder={user?.email} /> 
-                            </div>
+                                {/* name */}
+                                <label className="label">User Name</label>
 
 
-                            {/* password */}
-                            <label className="label">Property Name</label>
-                            <input type="text" name='propertyName' className="input rounded-full w-full" placeholder="Property Name" />
-                            {/* password */}
-                            <label className="label">Description</label>
-                            <input type="text" name='description' className="input rounded-full w-full" placeholder="Description" />
-                            {/* password */}
-                            <label className="label">Category</label>
-                            <input type="dropdown" className="input rounded-full w-full" placeholder="" />
-                            {/* password */}
-                            <label className="label">Price </label>
-                            <input type="text" name='price' className="input rounded-full w-full" placeholder="price" />
-                            {/* password */}
-                            <label className="label">Location </label>
-                            <input type="text" name='location' className="input rounded-full w-full" placeholder="location" />
+
+                                <input type="text" name='name'
+                                    className="input rounded-full w-full  "
+                                    readOnly
+                                    defaultValue={user.displayName} placeholder='' />
+                                {/* email */}
+                                <label className="label">Email Address</label>
+                                <div className='flex items-center relative '>
+                                    {/* <MdOutlineMailOutline size={22} className='text-gray-600 absolute top-1/2 left-3 -translate-y-1/2' /> */}
+                                    <input type="email"
+                                        readOnly
+                                        defaultValue={user?.email}
+                                        className="input rounded-full w-full read-only " name='email '
+                                        placeholder={user?.email} />
+                                </div>
 
 
-                            <button  className="btn btn-primary rounded-full mt-4">Add Property</button>
+                                {/* password */}
+                                <label className="label">Property Name</label>
+                                <input type="text" name='propertyName' required
+                                 className="input rounded-full w-full" placeholder="Property Name" />
+                                {/* password */}
+                                <label className="label">Description</label>
+                                <input type="text" name='description'  required
+                                className="input rounded-full w-full" placeholder="Description" />
+                                {/* password */}
+                                <label className="label">Category</label>
+                                <input type="dropdown" className="input rounded-full w-full" placeholder="" />
+                                {/* password */}
+                                <label className="label">Price </label>
+                                <input type="text" name='price'  required
+                                className="input rounded-full w-full" placeholder="price" />
+                                {/* password */}
+                                <label className="label">Location </label>
+                                <input type="text" name='location' required
+                                className="input rounded-full w-full" placeholder="location" />
 
-                        </fieldset>
-                       </form>
+
+                                <button className="btn btn-primary rounded-full mt-4">Add Property</button>
+
+                            </fieldset>
+                        </form>
                     </div>
                 </div>
             </div>
+           
         </div>
+         {/* add properties */}
+            <div className='mx-20 py-5'>
+                <h1 className='text-3xl font-medium'>All Add Properties Details</h1>
+            </div>
+       </div>
     );
 };
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,11 +5,11 @@ import { AuthContext } from '../context/AuthContext';
 
 const Navbar = () => {
     const links = <>
-        <NavLink to="/" className="mr-3 mb-3 lg:mb-0 text-[15px] text-[#0c121c] font-regular"><li>Home</li></NavLink>
-        <NavLink to="/allProperties" className="mr-3 mb-3 lg:mb-0 text-[15px] text-[#0c121c] font-regular"><li>All Properties</li></NavLink>
-        <NavLink to="/addProperties" className="mr-3 mb-3 lg:mb-0 text-[15px] text-[#0c121c] font-regular"><li>Add Properties</li></NavLink>
-        <NavLink to="/myProperties" className="mr-3 mb-3 lg:mb-0 text-[15px] text-[#0c121c] font-regular"><li>My Properties</li></NavLink>
-        <NavLink to="/myRatings" className="mr-3 mb-3 lg:mb-0 text-[15px] text-[#0c121c] font-regular"><li>My Ratings</li></NavLink>
+        <NavLink to="/" className="mr-3 mb-3 lg:mb-3 text-[15px] text-[#0c121c] font-regular"><li>Home</li></NavLink>
+        <NavLink to="/allProperties" className="mr-3 mb-3 lg:mb-3 text-[15px] text-[#0c121c] font-regular"><li>All Properties</li></NavLink>
+        <NavLink to="/addProperties" className="mr-3 mb-3 lg:mb-3 text-[15px] text-[#0c121c] font-regular"><li>Add Properties</li></NavLink>
+        <NavLink to="/myProperties" className="mr-3 mb-3 lg:mb-3 text-[15px] text-[#0c121c] font-regular"><li>My Properties</li></NavLink>
+        <NavLink to="/myRatings" className="mr-3 mb-3 lg:mb-3 text-[15px] text-[#0c121c] font-regular"><li>My Ratings</li></NavLink>
     </>
 
     const { user, signOutUser } = use(AuthContext)
@@ -24,8 +24,31 @@ const Navbar = () => {
 
         <div className="navbar   pl-4  ">
             <div className="navbar-start">
+
+
+
+                <div className="">
+                    <input id="my-drawer-1" type="checkbox" className="drawer-toggle" />
+                    <div className="drawer-content">
+                        
+                        
+                        <label htmlFor="my-drawer-1" className="">
+                               <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 curp" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
+
+                        </label>
+                    </div>
+                    <div className="drawer-side">
+                        <label htmlFor="my-drawer-1" aria-label="close sidebar" className="drawer-overlay"></label>
+                        <ul className="menu bg-base-200 min-h-full w-80 p-4">
+                           
+                            {links}
+                        </ul>
+                    </div>
+                </div>
+
+
                 <div className="dropdown">
-                    <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                    {/* <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
                     </div>
                     <ul
@@ -34,13 +57,13 @@ const Navbar = () => {
                         {links}
                         <Link to="/auth/logIn" className="btn mr-5 mb-3 lg:mb-0 btn-outline btn-primary">LogIn</Link>
                         <Link to="/auth/register" className="btn btn-primary mr-5">Register</Link>
-                    </ul>
+                    </ul> */}
 
 
 
 
                 </div>
-                <a href='/' className=" text-2xl text-center font-semibold  ">Home Nest</a>
+                <a href='/' className=" text-2xl text-center font-semibold ml-4 ">Home Nest</a>
 
             </div>
 
@@ -50,36 +73,36 @@ const Navbar = () => {
                 </ul>
                 {
                     user ?
-                <div className="flex gap-2">
+                        <div className="flex gap-2">
 
-                    <div className="dropdown dropdown-end">
-                        <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
-                            <div className="w-10 rounded-full">
-                                <img
-                                    alt="Tailwind CSS Navbar component"
-                                    src={user?.photoURL} />
+                            <div className="dropdown dropdown-end">
+                                <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
+                                    <div className="w-10 rounded-full">
+                                        <img
+                                            alt="Tailwind CSS Navbar component"
+                                            src={user?.photoURL} />
+                                    </div>
+                                </div>
+                                <ul
+                                    tabIndex="-1"
+                                    className="menu menu-md dropdown-content bg-base-100 rounded-box z-2 mt-3 w-70 p-2 shadow">
+                                    <li className=''>
+                                        <a className="justify-between mb-3 ">
+                                            {user?.displayName}
+                                            <span className="badge">New</span>
+                                        </a>
+                                    </li>
+                                    <li><a className='mb-3 p-2'>{user?.email}</a></li>
+                                    <button onClick={handleLogOut} className='btn btn-sm btn-outline btn-primary '>LogOut</button>
+
+                                </ul>
                             </div>
                         </div>
-                        <ul
-                            tabIndex="-1"
-                            className="menu menu-md dropdown-content bg-base-100 rounded-box z-2 mt-3 w-70 p-2 shadow">
-                            <li className=''>
-                                <a className="justify-between mb-3 ">
-                                    {user?.displayName}
-                                    <span className="badge">New</span>
-                                </a>
-                            </li>
-                            <li><a className='mb-3 p-2'>{user?.email}</a></li>
-                            <button onClick={handleLogOut} className='btn btn-sm btn-outline btn-primary '>LogOut</button>
-
-                        </ul>
-                    </div>
-                </div>
-                :
-                <>
-                    <Link to="/auth/logIn" className="btn btn-sm mr-4 btn-outline btn-primary">LogIn</Link>
-                    <Link to="/auth/register" className="btn btn-sm btn-primary ">Register</Link>
-                </>
+                        :
+                        <>
+                            <Link to="/auth/logIn" className="btn btn-sm mr-4 btn-outline btn-primary">LogIn</Link>
+                            <Link to="/auth/register" className="btn btn-sm btn-primary ">Register</Link>
+                        </>
                 }
 
             </div>
